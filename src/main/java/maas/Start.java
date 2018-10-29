231104461459e6b61c79b190b2fb92f0bbcb2db3
widya <widya.aulia280988@gmail.com>
2018-10-29 13:04:57 +0100

    update to have 20 buyers and 3 sellers

--- src/main/java/maas/Start.java
+++ src/main/java/maas/Start.java
@@ -2,20 +2,34 @@ package maas;
 
 import java.util.List;
 import java.util.Vector;
-import maas.tutorials.BookBuyerAgent;
 
 public class Start {
-    public static void main(String[] args) {
-    	List<String> agents = new Vector<>();
-    	agents.add("tester:maas.tutorials.BookBuyerAgent");
+	public static void main(String[] args) {
 
-    	List<String> cmd = new Vector<>();
-    	cmd.add("-agents");
-    	StringBuilder sb = new StringBuilder();
-    	for (String a : agents) {
-    		sb.append(a);
-    		sb.append(";");
-    	}
+		int num_buyers = 20;
+		int num_sellers = 3;
+
+		List<String> buyers = new Vector<>();
+		for (int i = 0; i < num_buyers; i++) {
+			buyers.add("Buyer" + Integer.toString(i) + ":maas.tutorials.BookBuyerAgent");
+		}
+	
+		List<String> sellers = new Vector<>();
+		for (int i = 0; i < num_buyers; i++) {
+			sellers.add("Seller" + Integer.toString(i) + ":maas.tutorials.BookSellerAgent");
+		}
+
+		List<String> cmd = new Vector<>();
+		cmd.add("-agents");
+		StringBuilder sb = new StringBuilder();
+		for (String a : buyers) {
+			sb.append(a);
+			sb.append(";");
+		}
+		for (String a : sellers) {
+			sb.append(a);
+			sb.append(";");
+		}
     	cmd.add(sb.toString());
         jade.Boot.main(cmd.toArray(new String[cmd.size()]));
     }
